b8d5412c3814ce2ea6b06930586f236b498f949d
Jhonas Aparecido Alves Do Nascimento <jhonasnascimento@brq.com>
2016-03-17 13:48:39 -0300

    retirado implementação antiga de QRCode, gen e scan

--- www/js/base/util.js
+++ www/js/base/util.js
@@ -10,35 +10,8 @@ FlashBuy.util = {
       $(document).ready(callback);
     }
   },
-  QRCode: {
-    scan: function (callback) {
-      var success = function(success) {
-        callback(true, success);
-      };
-      var error = function(error) {
-        callback(false, error);
-      };
-
-      cordova.plugins.barcodeScanner.scan(
-        success,
-        error
-      );
-    },
-    generate: function (information, callback) {
-      var success = function(success) {
-        callback(true, success);
-      };
-      var error = function(error) {
-        callback(false, error);
-      };
-
-      cordova.plugins.barcodeScanner.encode(
-        cordova.plugins.barcodeScanner.Encode.TEXT_TYPE,
-        information,
-        success,
-        error
-      );
-    }
+  gerarQRCode: function () {
+      //implementar...
   },
   getDeviceId: function () {
     return device.uuid;
