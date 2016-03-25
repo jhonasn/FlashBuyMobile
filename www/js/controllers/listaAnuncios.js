0f84e452327dc6b69cf1bb36d19636c58a8cf663
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-03-25 17:07:49 -0300

    removidos os anuncios direto no html, agora são carregados através do json

--- www/js/controllers/listaAnuncios.js
+++ www/js/controllers/listaAnuncios.js
@@ -2,11 +2,20 @@
 /// <reference path="js/base/app.js" />
 /// <reference path="js/base/util.js" />
 
+//temporário
+
+
+
 FlashBuy.listaAnuncios = {
     init: function () {
         console.log('listaAnuncios init');
+
+       
     },
     ready: function () {
         console.log('listaAnuncios ready');
+
+        //método de chamar a lista de anúncios
+        FlashBuy.util.getListaAnuncios();
     }
 };
