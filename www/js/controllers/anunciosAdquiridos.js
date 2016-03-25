4face0468bc9defc891c27c27505fbb8af5140b0
José Victor Veronez Andrade <jozeveronez@gmail.com>
2016-03-25 15:06:26 -0300

    Adicionada a página de Anuncios Adquiridos

---
+++ www/js/controllers/anunciosAdquiridos.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="js/base/base.js" />
+/// <reference path="js/base/app.js" />
+/// <reference path="js/base/util.js" />
+
+FlashBuy.anunciosAdquiridos = {
+    init: function () {
+        console.log('anunciosAdquiridos init');
+    },
+    ready: function () {
+        console.log('anunciosAdquiridos ready');
+    }
+};
+
