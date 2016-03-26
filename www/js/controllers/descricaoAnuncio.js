543e9a161e98d0d16e6c0c2a98670f2d4d7649d7
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-03-25 22:24:46 -0300

    Adicionado controller descricaoAnuncio

---
+++ www/js/controllers/descricaoAnuncio.js
@@ -0,0 +1,9 @@
+﻿FlashBuy.descricaoAnuncio = {
+    init: function (teste, outro) {
+        console.log('teste: ' + teste + ', outro: ' + outro);
+        console.log('descricaoAnuncio init');
+    },
+    ready: function () {
+        console.log('descricaoAnuncio ready');       
+    }
+};
