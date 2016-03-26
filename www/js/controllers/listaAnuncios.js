35cffda84044b021ca9ec690c094ae676a736a60
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-03-25 22:31:36 -0300

    pequena alteração para fazer a listagem

--- www/js/controllers/listaAnuncios.js
+++ www/js/controllers/listaAnuncios.js
@@ -4,12 +4,12 @@
 
 FlashBuy.listaAnuncios = {
     init: function () {
-        console.log('listaAnuncios init');       
+        console.log('listaAnuncios init');
     },
     ready: function () {
         console.log('listaAnuncios ready');
 
-        //método de chamar a lista de anúncios
+        //método de chamar a lista de anúncios fake
         FlashBuy.util.getListaAnuncios();
     }
 };
