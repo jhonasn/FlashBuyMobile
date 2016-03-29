5788b4f1b85e7b9f5871ca5c214373c2279bc282
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-03-29 20:08:03 -0300

    Adicionado o qrCode no controller descricaoAnuncio

--- www/js/controllers/listaAnuncios.js
+++ www/js/controllers/listaAnuncios.js
@@ -4,44 +4,43 @@
 
 FlashBuy.listaAnuncios = {
     init: function () {
+        //método de chamar a lista de anúncios fake
+        FlashBuy.listaAnuncios.getListaAnuncios(function () {
+            FlashBuy.util.configurarRotasControllers();
+        });
         console.log('listaAnuncios init');
     },
     ready: function () {
-        //metodo para carregar o json listaAnunciosFake
-        function getListaAnuncios(cb) {
-            $.getJSON('data/listaAnunciosFake.json', function (listaDeAnuncios) {
-                $.each(listaDeAnuncios.anunciosFake, function (index, oferta) {
-                    $("#divOfertas").append(
-                        "<div class='oferta'>" +
-                            "<div class='col s12 m7'>" +
-                                "<div class='card small'>" +
-                                "<!-- Imagem do card, por enquanto vou deixar sem -->" +
-                                    "<div class='card-image'>" +
-                                        "<img src='../img/semImagem.png'>" +
-                                        "<span class='card-title'>" + oferta.Produto + "</span>" +
-                                    "</div>" +
-                                    "<div class='card-content'>" +
-                                        "<p>" + oferta.Descricao + "</p>" +
-                                    "</div>" +
-                                    "<div class='card-action'>" +
-                                        "<button type='button' data-controller='descricaoAnuncio' data-idOferta='" + oferta.IdOferta + "'>Abrir Oferta</button>" +
-                                        "<a href='#'>Recusar</a>" +
-                                    "</div>" +
+        console.log('listaAnuncios ready');
+    },
+    //metodo para carregar o json listaAnunciosFake
+    getListaAnuncios: function (cb) {
+        $.getJSON('data/listaAnunciosFake.json', function (listaDeAnuncios) {
+            $.each(listaDeAnuncios.anunciosFake, function (index, oferta) {
+                $("#divOfertas").append(
+                    "<div class='oferta'>" +
+                        "<div class='col s12 m7'>" +
+                            "<div class='card small'>" +
+                            "<!-- Imagem do card, por enquanto vou deixar sem -->" +
+                                "<div class='card-image'>" +
+                                    "<img src='../img/semImagem.png'>" +
+                                    "<span class='card-title'>" + oferta.Produto + "</span>" +
+                                "</div>" +
+                                "<div class='card-content'>" +
+                                    "<p>" + oferta.Descricao + "</p>" +
+                                "</div>" +
+                                "<div class='card-action'>" +
+                                    "<button type='button' data-controller='descricaoAnuncio' data-idOferta='" + oferta.IdOferta + "'>Abrir Oferta</button>" +
+                                    "<a href='#'>Recusar</a>" +
                                 "</div>" +
                             "</div>" +
-                        '</div>');
-                });
-
-                if (cb) {
-                    cb();
-                }
+                        "</div>" +
+                    '</div>');
             });
-        }
-        console.log('listaAnuncios ready');
 
-        //método de chamar a lista de anúncios fake
-        getListaAnuncios(function () {
-            FlashBuy.util.configurarRotasControllers();
+            if (cb) {
+                cb();
+            }
         });
     }
 };
