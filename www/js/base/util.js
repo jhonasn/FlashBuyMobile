e146c6e16ff6fc75455ad36ae3a265d27ec1400b
gu-silva <tribodopaje@gmail.com>
2016-03-29 18:29:13 -0300

    Conclusão do método conectadoInternet() e Adicionado método tipoInternet().

--- www/js/base/util.js
+++ www/js/base/util.js
@@ -72,9 +72,19 @@ FlashBuy.util = {
     },
     conectadoInternet: function ()
     {
-        alert(navigator.connection.type);
+        if (navigator.connection.type != Connection.NONE) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     },
- 
+
+    tipoInternet: function () {
+        return navigator.connection.type;
+    },
+
     configurarRotasControllers: function () {
         //configura chamada de telas / rotas dentro da tela carregada
         var $content = $('#content');
