4e8363ad2d1ae0f6ffe5ce6394b1306ce001868a
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-04-01 20:24:40 -0300

    Implementado busca através do web service

--- www/js/controllers/descricaoAnuncio.js
+++ www/js/controllers/descricaoAnuncio.js
@@ -3,30 +3,22 @@
 /// <reference path="js/base/util.js" />
 
 FlashBuy.descricaoAnuncio = {
-    init: function (idOferta) {
-        console.log('idOferta: ' + idOferta);
-        //chamada do método
-        FlashBuy.descricaoAnuncio.getListaAnuncios(idOferta);
+    init: function (idOferta, DataInicio, DataFim, produto, idAnunciante) {
         console.log('descricaoAnuncio init');
+
+        var stringQrCode = String(idOferta);
+        //chamada do qrCode
+        FlashBuy.descricaoAnuncio.gerarQrCode(stringQrCode, 'qrcode');
+
+        //jogando as informações na tela, podemos implementar um template depois
+        $("#descricaoOferta").append("<p> idOferta: " + idOferta + "</p>");
+        $("#descricaoOferta").append("<p> produto: " + produto + "</p>");
+        $("#descricaoOferta").append("<p> dataInicio: " + DataInicio + "</p>");
+        $("#descricaoOferta").append("<p> dataFim: " + DataFim + "</p>");
+        $("#descricaoOferta").append("<p> idAnunciante: " + idAnunciante + "</p>");
     },
     ready: function () {
         console.log('descricaoAnuncio ready');
-    },
-    //metodo para carregar a oferta através do id,
-    //para fim de testes apenas, logo implementaremos via web service.
-    getListaAnuncios: function (idOferta) {
-        $.getJSON('data/listaAnunciosFake.json', function (listaDeAnuncios) {
-            $.each(listaDeAnuncios.anunciosFake, function (index, oferta) {
-                if (idOferta === oferta.IdOferta) {
-                    $("#descricaoOferta").append("<p> id: " + oferta.IdOferta + "</p>");
-                    $("#descricaoOferta").append("<p> descricao: " + oferta.Descricao + "</p>");
-                    $("#descricaoOferta").append("<p> local: " + oferta.LocalOferta + "</p>");
-
-                    //chamada do qrCode
-                    FlashBuy.descricaoAnuncio.gerarQrCode(oferta.Produto, 'qrcode');
-                }
-            });
-        });
     },
     gerarQrCode: function (string, divId) {
         FlashBuy.util.gerarQRCode(string, divId);
