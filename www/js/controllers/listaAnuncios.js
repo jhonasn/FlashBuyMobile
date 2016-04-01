1ac7c7b6e034d275c65efc3ac9680e7e4233ce5b
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-04-01 13:24:11 -0300

    Implementado busca e listagem através do web service

--- www/js/controllers/listaAnuncios.js
+++ www/js/controllers/listaAnuncios.js
@@ -12,48 +12,27 @@ FlashBuy.listaAnuncios = {
     },
     ready: function () {
         console.log('listaAnuncios ready');
-    },
-    //faz a comunicação com o web service
-    getListaAnuncios: function () {
+    },    
+    //metodo para carregar o json listaAnunciosFake
+    carregarAnuncios: function (cb) {
+        //COMO CARREGAR DENTRO DA VARIAVEL models O OBJETO data DA FUNCAO getListaAnuncios???
         $.get('http://189.16.45.2/flashbuywebapi/api/Ofertas/GetOferta')
         .success(function (data) {
             console.info('proxy ok!');
             console.log(data);
+            htmlTemplate = FlashBuy.util.getHtml('views/listaAnunciosTemplate.html');
+
+            data.forEach(function (model) {
+                var htmlRenderizado = FlashBuy.util.templateHtml(htmlTemplate, model);
+                $('#divOferta').append(htmlRenderizado);
+            });
+
+            if (cb) {
+                cb();
+            }
         })
         .error(function () {
             console.error(arguments);
         });
-    },
-    //metodo para carregar o json listaAnunciosFake
-    carregarAnuncios: function (cb) {
-        //COMO CARREGAR DENTRO DA VARIAVEL models O OBJETO data DA FUNCAO getListaAnuncios???
-        var models = [
-           {
-               produto: 'Teste de produto 2, titulo 1',
-               idOferta: '001',
-               //descricao: 'Descrição de teste que se repete do teste 2/1'
-           },
-            {
-                produto: 'Teste de produto, titulo 2',
-                idOferta: '002',
-                //descricao: 'Descrição de teste que se repete do teste 2/2'
-            },
-            {
-                produto: 'Teste de produto, titulo 3',
-                idOferta: '003',
-                //descricao: 'Descrição de teste que se repete do teste 2/3'
-            }
-        ];
-
-        htmlTemplate = FlashBuy.util.getHtml('views/listaAnunciosTemplate.html');
-
-        models.forEach(function (model) {
-            var htmlRenderizado = FlashBuy.util.templateHtml(htmlTemplate, model);
-            $('#divOferta').append(htmlRenderizado);
-        });
-
-        if (cb) {
-            cb();
-        }
     }
 };
