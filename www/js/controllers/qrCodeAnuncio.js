b150f97fdfcff6f46156ae9d42e8f56331b74303
EduardoGarrett <eduardo-garrett-miranda@hotmail.com>
2016-04-05 08:48:05 -0300

    Adicionado controller qrCodeAnuncio

---
+++ www/js/controllers/qrCodeAnuncio.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="js/base/base.js" />
+/// <reference path="js/base/app.js" />
+/// <reference path="js/base/util.js" />
+
+FlashBuy.qrCodeAnuncio = {
+    init: function (qrcode) {
+        console.log('qrCode: ' + qrcode);
+
+        //Conversão da variável em string necessária, se nao tiver, o qrCode nao funciona
+        qrcode = String(qrcode);
+
+        $("#qrcode").empty();
+        FlashBuy.util.gerarQRCode(qrcode, "qrcode");
+
+
+        console.log('qrCodeAnuncio init');
+    },
+    ready: function () {
+        console.log('qrCodeAnuncio ready');
+    }
+};
