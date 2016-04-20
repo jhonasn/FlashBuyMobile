61d95e929d35e1a94a4af0fcffc1d7246f6d7dac
Gustavo Henrique Silva <homem_gu@hotmail.com>
2016-04-20 17:43:02 -0300

    Adicionando a base para trabalhar com o login (login.js, login.html, ref na index.html e detalhe na app.js).

---
+++ www/js/controllers/login.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../base/base.js" />
+/// <reference path="../base/app.js" />
+/// <reference path="../base/util.js" />
+
+FlashBuy.login = {
+    init: function () {
+        console.log('login init');
+    },
+    ready: function () {
+        console.log('login ready');
+    }
+};
